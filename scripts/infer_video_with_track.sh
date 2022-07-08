aaaca52efd16b8fc4ebae3e64f54001a9e352bb3
Haotianz94 <haotianz@stanford.edu>
2022-07-07 23:21:55 -0700

    update script

--- scripts/infer_video_with_track.sh
+++ scripts/infer_video_with_track.sh
@@ -2,7 +2,7 @@
 SCRIPT=/raid/haotian/Projects/lift3d/external/hybrik/infer_video.py 
 VIDEO_DIR=/raid/jhong/tennis-video
 TRACK_DIR=/home/james/lift3d/data/tennis_track
-OUT_DIR=/raid/haotian/Projects/lift3d/output/tennis_pose_hybrik
+OUT_DIR=/raid/haotian/Projects/lift3d/output/tennis_pose_hybrik_new
 
 CUDA_VISIBLE_DEVICES=2 python $SCRIPT $VIDEO_DIR $TRACK_DIR $OUT_DIR --part 0 6 & 
 
@@ -15,3 +15,5 @@ CUDA_VISIBLE_DEVICES=5 python $SCRIPT $VIDEO_DIR $TRACK_DIR $OUT_DIR --part 3 6 
 CUDA_VISIBLE_DEVICES=6 python $SCRIPT $VIDEO_DIR $TRACK_DIR $OUT_DIR --part 4 6 & 
 
 CUDA_VISIBLE_DEVICES=7 python $SCRIPT $VIDEO_DIR $TRACK_DIR $OUT_DIR --part 5 6 & 
+
+wait
